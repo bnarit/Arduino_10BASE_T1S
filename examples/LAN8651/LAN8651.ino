116d847c8747a49f82c5c5e530a016933df7efc7
Alexander Entinger <consulting@lxrobotics.com>
2023-07-05 10:29:49 +0200

    Drop no longer required commented out code.

--- examples/LAN8651/LAN8651.ino
+++ examples/LAN8651/LAN8651.ino
@@ -21,18 +21,6 @@ void setup()
   Serial.begin(115200);
   while (!Serial) { }
 
-  delay(1000);
-
-//  uint8_t mac_addr[6] = {0};
-//  if (!TC6Stub_Init(0, mac_addr)) {
-//    Serial.println("TC6Stub_Init failed.");
-//    return;
-//  }
-//
-//  char msg[32] = {0};
-//  snprintf(msg, sizeof(msg), "MAC = %02X:%02X:%02X:%02X:%02X:%02X", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-//  Serial.println(msg);
-
   lwip_idx = TC6LwIP_Init(IP,
                           T1S_PLCA_ENABLE,
                           T1S_PLCA_NODE_ID,
