283c692dc6474ba246dd0a0d1a5fb3245c8d43c8
Alexander Entinger <consulting@lxrobotics.com>
2024-03-11 07:33:49 +0100

    It is pretter to print an error message for Uno than to pretend that you can use this library with the Arduino Uno.

--- examples/LAN8651-iperf/LAN8651-iperf.ino
+++ examples/LAN8651-iperf/LAN8651-iperf.ino
@@ -41,7 +41,7 @@ static T1SMacSettings const t1s_mac_settings{MAC_PROMISCUOUS_MODE, MAC_TX_CUT_TH
 static int const CS_PIN    = 10;
 static int const RESET_PIN =  9;
 static int const IRQ_PIN   =  2;
-#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_MINIMA) || defined(ARDUINO_UNOWIFIR4) || defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_MINIMA) || defined(ARDUINO_UNOWIFIR4) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
 /* Those are all boards with the Arduino Uno form factor for the T1S shield. */
 static int const CS_PIN    =  9;
 static int const RESET_PIN =  4;
